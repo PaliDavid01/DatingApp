2b70c985a129419b829570cb0a7cd3e8e4e6c86b
Páli Dávid <pali.david2001@outlook.com>
2023-08-26 23:19:28 +0200

    Backbone is created

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController:ControllerBase
+{
+    private readonly DataContext _context;
+
+    public UserController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetAll(){
+        return await _context.Users.ToListAsync();
+    }
+    
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id){
+        return await _context.Users.FindAsync(id);
+    }
+}
